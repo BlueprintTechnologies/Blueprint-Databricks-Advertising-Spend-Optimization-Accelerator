f365286e7b1edb4e7563baf61b38998484772862
desnaurrizar@bpcs.com <desnaurrizar@bpcs.com>
2022-04-26 01:24:28 +0000

    Notebook Clean up 003

--- X-Challenge Apr 1/ad_spend_optimizer/03b_prep_training_data_NEW.py
+++ X-Challenge Apr 1/ad_spend_optimizer/03b_prep_training_data_NEW.py
@@ -49,11 +49,8 @@ input_table_name = "bronze_with_clicks_and_cost"
 bronze_clicks = spark.table(input_table_name)
 bronze_clicks.createOrReplaceTempView("bronze_clicks")
 print(bronze_clicks)
-display(bronze_clicks)
-
-# COMMAND ----------
-
 print(bronze_clicks.count())
+display(bronze_clicks)
 
 # COMMAND ----------
 
@@ -68,19 +65,12 @@ user_visits= spark.sql("""SELECT uid, time, channel, product_stars,
                          avg(gpt) OVER (PARTITION BY uid,channel, product_stars) AS gpt
                     FROM bronze_clicks""").drop_duplicates()
 #display(visits.filter(" uid = '00032268f59b4867bc1c4143de39de3c'"))
+#print(user_visits.count())
 display(user_visits)
 
 # COMMAND ----------
 
-print(user_visits.count())
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * FROM bronze_with_clicks_and_cost WHERE uid = '00454ed87fed4c2e8844ad1aa0130b67'
-
-# COMMAND ----------
-
+# DBTITLE 1,Show Marketing Spend Table from Original Databricks Accelerator
 # MAGIC %sql
 # MAGIC SELECT * FROM gold_ad_spend
 
@@ -126,7 +116,7 @@ contribution_per_customer.printSchema()
 
 # COMMAND ----------
 
-# DBTITLE 1,Rank Customers by how active/visits
+# DBTITLE 1,Rank Customers by how active/visits - Part 1
 #%sql
 df = spark.sql("""
     (SELECT 
@@ -147,19 +137,7 @@ display(df)
 
 # COMMAND ----------
 
-# DBTITLE 1,Rank users according to how active/visits
-# MAGIC %sql
-# MAGIC SELECT VISITS_RANK, SUM(sum_visit_clicks) as visits
-# MAGIC FROM 
-# MAGIC (SELECT 
-# MAGIC       NTILE(100) OVER ( ORDER BY sum_visit_impr DESC) AS VISITS_RANK,
-# MAGIC       --NTILE(10) OVER (ORDER BY sum_visit_clicks DESC) AS CLICKS_RANK,
-# MAGIC       *
-# MAGIC FROM  contribution_per_customer) AS A
-# MAGIC GROUP BY VISITS_RANK
-
-# COMMAND ----------
-
+# DBTITLE 1,Rank Customers by how active/visits - Part 2
  df2 = spark.sql("""
  SELECT VISIT_RANK, visits, DENSE_RANK() OVER (ORDER BY visits) AS VISIT_RANK2 
  FROM 
@@ -178,12 +156,13 @@ display(df2)
 
 # COMMAND ----------
 
+# DBTITLE 1,Rank Customers by how active/visits - Part 3
 df3 = df.join(df2.select('VISIT_RANK', 'VISIT_RANK2'), on = ["VISIT_RANK"], how = 'left')
 display(df3.select('VISIT_RANK', 'VISIT_RANK2').distinct().sort(col('VISIT_RANK2'), col('VISIT_RANK')))
 
 # COMMAND ----------
 
-# DBTITLE 1,Rank Users by how active/visits 2
+# DBTITLE 1,Rank Customers by how active/visits - Part 4
 user_visit_rankings = df3.select('uid', 'sum_visit_impr', 'VISIT_RANK', 'VISIT_RANK2').distinct()
 max_impr = user_visit_rankings.agg({"sum_visit_impr": "max"}).collect()[0]
 min_impr = user_visit_rankings.agg({"sum_visit_impr": "min"}).collect()[0]
@@ -193,6 +172,18 @@ print(max_impr,min_impr)
 print(max_rank, min_rank)
 display(user_visit_rankings.sort(col('VISIT_RANK2').desc()))
 #display(user_visit_rankings.select('VISIT_RANK', 'VISIT_RANK2').distinct().sort(col('VISIT_RANK2'), col('VISIT_RANK')))
+
+# COMMAND ----------
+
+# DBTITLE 1,Save to Delta
+user_visit_rank_table_name = 'user_visit_rankings'
+
+(user_visit_rankings
+      .write
+      .format("delta")
+      .mode("overwrite")
+      .saveAsTable(user_visit_rank_table_name)
+)
 
 # COMMAND ----------
 
@@ -244,6 +235,7 @@ display(contribution_per_channel_detail)
 
 # COMMAND ----------
 
+# DBTITLE 1,Number of Interactions and Customers per Channel
 # MAGIC %sql
 # MAGIC SELECT A.channel, A.sum_visit_clicks, B.ncust
 # MAGIC FROM
@@ -270,31 +262,16 @@ display(contribution_per_channel_detail)
 
 # COMMAND ----------
 
-display(bronze_data2)
-
-# COMMAND ----------
-
-print(bronze_data2.columns)
-
-# COMMAND ----------
-
-print(user_visit_rankings.select(['uid', 'sum_visit_impr', 'VISIT_RANK2']).columns)
-
-# COMMAND ----------
-
-# DBTITLE 1, RANK Users 2
+# DBTITLE 1,Join User Rankings with Click Data
 user_rankings = user_visit_rankings.select(['uid', 'sum_visit_impr', 'VISIT_RANK2'])
+#print(user_visit_rankings.select(['uid', 'sum_visit_impr', 'VISIT_RANK2']).columns)
 silver_data = bronze_data2.join(user_rankings, on = 'uid', how = 'left')
 print(silver_data.count())
 display(silver_data)
 
 # COMMAND ----------
 
-print(bronze_clicks.columns)
-
-# COMMAND ----------
-
-# DBTITLE 1,summarize user behavior2
+# DBTITLE 1,Summarize User Behavior - Part 2
 user_visits2= (spark.sql("""SELECT uid, 
                          count(interaction) OVER (PARTITION BY uid) AS user_impr,
                          sum(click) OVER (PARTITION BY uid) AS user_clicks,
@@ -315,7 +292,7 @@ user_visits3 = spark.sql("""
                              sum(user_txns) as user_txns,
                              avg(user_cpi) as user_cpi,
                              max(user_cpc) as user_cpc,
-                             avg(user_gpt) as user_gpt,
+                             avg(user_gpt) as user_gpt
                              --sum(user_gpt) as sum_user_gpt
                              --std(user_gpt) as std_gpt
                          FROM
@@ -330,27 +307,14 @@ display(user_visits3)
 
 # COMMAND ----------
 
-display(user_visit_rankings)
-
-# COMMAND ----------
-
-user_visit_rank_table_name = 'user_visit_rankings'
-
-(user_visit_rankings
-      .write
-      .format("delta")
-      .mode("overwrite")
-      .saveAsTable(user_visit_rank_table_name)
-)
-
-# COMMAND ----------
-
+# DBTITLE 1,Summarize User Behavior - Part 3
 user_visits4 = user_visits3.join(user_visit_rankings.select('uid', 'VISIT_RANK2'), on = 'uid', how = 'left')
 user_visits4.createOrReplaceTempView("user_visits4")
 display(user_visits4)
 
 # COMMAND ----------
 
+# DBTITLE 1,Summarize User Behavior - Part 4
 user_visits5 = spark.sql(
 """
 select VISIT_RANK2, sum(user_impr) impr, sum(user_clicks) clicks, sum(user_txns) txns,
@@ -369,6 +333,7 @@ spark.sql("DROP TABLE IF EXISTS users_revenue_per_segment")
 
 # COMMAND ----------
 
+# DBTITLE 1,Save to Delta Lake
 out_table_name = 'users_revenue_per_segment'
 (user_visits5
      .write
@@ -381,10 +346,11 @@ out_table_name = 'users_revenue_per_segment'
 
 # MAGIC %sql
 # MAGIC select * from users_revenue_per_segment
-# MAGIC order by gp
+# MAGIC order by potential_gp
 
 # COMMAND ----------
 
+# DBTITLE 1,Summarize User Behavior - Part 5
 #%sql
 df10 = spark.sql("""
     (SELECT 
@@ -426,6 +392,7 @@ print(silver_clicks_raw.columns)
 
 # COMMAND ----------
 
+# DBTITLE 1,Summarize User Behavior - Part 6
 silver_clicks2 = spark.sql("""
 SELECT uid, channel, product_stars,
        month, dayofweek, weekofyear,
@@ -451,6 +418,7 @@ display(silver_clicks2.sort('uid', 'month', 'weekofyear', 'dayofweek' ))
 
 # COMMAND ----------
 
+# DBTITLE 1,Save to Delta Lake
 silver_table_name2 = "silver_with_clicks_and_cost2"
 
 (silver_clicks2
@@ -476,7 +444,7 @@ silver_table_name2 = "silver_with_clicks_and_cost2"
 # MAGIC SELECT 
 # MAGIC     month, weekofyear, dayofweek, uid, first(channel) as channel, avg(product_stars), sum(impr), first(total_impr), sum(clicks), sum(txns), avg(cpm), avg(cpc), avg(gpt)
 # MAGIC FROM
-# MAGIC    silver_clicks1
+# MAGIC    silver_with_clicks_and_cost2
 # MAGIC GROUP BY 
 # MAGIC     month, weekofyear, dayofweek, uid
 # MAGIC ORDER BY 
@@ -484,12 +452,4 @@ silver_table_name2 = "silver_with_clicks_and_cost2"
 
 # COMMAND ----------
 
-print(silver_clicks.columns)
-
-# COMMAND ----------
-
-display(silver_clicks.filter("clicks > impr"))
-
-# COMMAND ----------
-
-
+display(silver_clicks2.filter("clicks > impr"))
